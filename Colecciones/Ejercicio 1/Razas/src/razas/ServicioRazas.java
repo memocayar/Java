cf73893c670d28b235c7f3b8f420dd1cec1ab98f
memocayar <mariaemiliamocayar@gmail.com>
2022-03-31 21:06:25 -0300

    31.03.22 2° ej

---
+++ Colecciones/Ejercicio 1/Razas/src/razas/ServicioRazas.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package razas;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Maria Emilia
+ */
+public class ServicioRazas {
+
+    private Scanner leer = new Scanner(System.in).useDelimiter("\n");
+    private ArrayList<String> razas = new ArrayList();
+
+    public ServicioRazas() {
+    }
+
+    public ServicioRazas(ArrayList<String> razas) {
+        this.razas = razas;
+    }
+    
+    @Override
+    public String toString() {
+        return "raza: " + this.razas;
+    }
+    
+    public void mostrarRazas() {
+        System.out.println("");
+        System.out.println("Las razas ingresadas son:");
+        for (String var : razas) {
+            System.out.println(var.toString());
+        }
+        System.out.println("");
+    }
+    
+    public void ingresarRaza() {
+        int rta = 1;
+        String aux = "";
+        do {
+            System.out.println("Ingresar raza del perro:");
+            aux = leer.nextLine();
+            razas.add(aux);
+
+            System.out.println("La raza fue agregada correctamente. ¿Desea agregar otra raza de perro?");
+            System.out.println("1- SI");
+            System.out.println("2- NO");
+            rta = leer.nextInt();
+
+            leer.nextLine();
+        } while (rta == 1);
+        
+        mostrarRazas();
+    }
+
+    public void eliminarRaza() {
+        String r = "";
+        System.out.println("Ingrese qué raza desea eliminar");
+        r = leer.next();
+
+        for (int i = 0; i < razas.size(); i++) {
+            if (razas.get(i).equals(r)) {
+                razas.remove(i);
+            }
+        }
+        
+        mostrarRazas();
+    }
+}
